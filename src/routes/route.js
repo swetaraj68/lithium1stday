527d2a3b254504408e248ef175e2b20064121dd6
swetaraj68 <swetakumariraj128@gmail.com>
2022-10-20 23:10:47 +0530

    players question is done

--- src/routes/route.js
+++ src/routes/route.js
@@ -69,5 +69,52 @@ router.post("/test-post-4", function(req, res) {
     arr.push(ele)
     res.send(  { msg: arr , status: true }  )
 })
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ]
+       },
+   ]
+   
+
+   router.post("/postapi", function(req, res) {
+    let abc = req.body.element
+    for (let i=0;i<players.length;i++){
+        if(i.name ===abc.name){
+       return res.send("already exits")
+        }
+    }
+        
+        players.push(abc)
+         res.send({msg :players , status :true  })
+        
+    }
+   )
+
+
 
 module.exports = router;
